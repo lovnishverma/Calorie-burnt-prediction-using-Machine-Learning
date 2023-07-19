cc335f292b8565e6ae046792f0fe9bc099f5e02a
Glitch (glitch-hello-website) <none>
2023-07-19 17:30:50 +0000

    🏁🌚 Checkpoint ./server.py:57629255/618 ./templates/calorie.html:57629255/53 ./requirements.txt:57629255/64 ./templates/index.html:57629255/25

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import *
 import pandas as pd
-import numpy as np
 from  sklearn.linear_model import LinearRegression 
+
 
 
 app = Flask(__name__) 
@@ -16,7 +16,7 @@ def predict():
 
 @app.route('/cp', methods = ['POST'] ) 
 def caloriesburntpredict(): 
-  # location='Whitefield'
+  # get input from user via form
   Age  = eval ( request.form.get ( "Age") )
   Duration=eval(request.form.get("Duration"))
   Heart_Rate  = eval ( request.form.get ( "Heart_Rate") )
@@ -26,23 +26,10 @@ def caloriesburntpredict():
   df = pd.read_csv(url)
   X = df.drop(['ID','Calories'],axis='columns')
   Y = df["Calories"]
-  from sklearn.linear_model import LinearRegression
   model = LinearRegression()
   model.fit(X,Y)
-#   loc_index = np.where(X.columns==location)[0][0]
-
-#   x = np.zeros(len(X.columns))
-#   x[1] = Age
-#   x[2] = Duration
-#   x[3] = Heart_Rate
-#   x[4] = Body_Temp
-#   if loc_index >= 0:
-#       x[loc_index] = 1
-        # ethe ki kariye ???
   arr=model.predict([[Age,Duration,Heart_Rate,Body_Temp]])
-  # hp = model.predict([X])
-  return " Calories Burnt "  + str(arr[0])
-
+  return  render_template("calorie.html", data=arr[0]) 
 
 if __name__ == '__main__': 
   app.run()
