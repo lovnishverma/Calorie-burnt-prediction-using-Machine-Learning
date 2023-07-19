fd64f691b36b5308588f31858f370549d2736666
Glitch (glitch-hello-website) <none>
2023-07-19 17:42:26 +0000

    🚄🦀 Checkpoint ./templates/index.html:57629255/5188 ./requirements.txt:57629255/6 ./server.py:57629255/2758

--- server.py
+++ server.py
@@ -1,35 +1,47 @@
 from flask import *
 import pandas as pd
-from  sklearn.linear_model import LinearRegression 
+from sklearn.linear_model import LinearRegression
 
+app = Flask(__name__)
 
+@app.route('/')
+def index():
+    # This function renders the index.html template when the root URL is accessed.
+    return render_template('index.html')
 
-app = Flask(__name__) 
+@app.route('/r')
+def predict():
+    # This function renders the calorie.html template when the '/r' URL is accessed.
+    # It opens the form for result prediction.
+    return render_template('calorie.html')
 
-@app.route('/') 
-def index(): 
-  return render_template('index.html') 
+@app.route('/cp', methods=['POST'])
+def caloriesburntpredict():
+    # This function is called when the form in calorie.html is submitted.
+    # It receives the input from the user via the form.
 
-@app.route('/r') # open the form for result prediction  
-def predict(): 
-  return  render_template('calorie.html') 
+    # Get input values from the form and convert them to appropriate data types.
+    Age = eval(request.form.get("Age"))
+    Duration = eval(request.form.get("Duration"))
+    Heart_Rate = eval(request.form.get("Heart_Rate"))
+    Body_Temp = eval(request.form.get("Body_Temp"))
 
-@app.route('/cp', methods = ['POST'] ) 
-def caloriesburntpredict(): 
-  # get input from user via form
-  Age  = eval ( request.form.get ( "Age") )
-  Duration=eval(request.form.get("Duration"))
-  Heart_Rate  = eval ( request.form.get ( "Heart_Rate") )
-  Body_Temp   = eval ( request.form.get ( "Body_Temp") )
-  # predict and save the output in result variable
-  url   = "cbp.csv"
-  df = pd.read_csv(url)
-  X = df.drop(['ID','Calories'],axis='columns')
-  Y = df["Calories"]
-  model = LinearRegression()
-  model.fit(X,Y)
-  arr=model.predict([[Age,Duration,Heart_Rate,Body_Temp]])
-  return  render_template("calorie.html", data=arr[0]) 
+    # Load the CSV file containing the data needed for the prediction.
+    url = "cbp.csv"
+    df = pd.read_csv(url)
+    X = df.drop(['ID', 'Calories'], axis='columns')
+    Y = df["Calories"]
 
-if __name__ == '__main__': 
-  app.run()
+    # Create a LinearRegression model and fit it to the data.
+    model = LinearRegression()
+    model.fit(X, Y)
+
+    # Predict the result for the input values and store it in the 'arr' variable.
+    arr = model.predict([[Age, Duration, Heart_Rate, Body_Temp]])
+
+    # Render the calorie.html template with the predicted result.
+    return render_template("calorie.html", data=arr[0])
+
+if __name__ == '__main__':
+    # This block of code runs the Flask application when the script is executed.
+    app.run()
